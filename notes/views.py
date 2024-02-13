b03a63f9e8e0f9ebca4db8a86c83d63de7f51607
Himangshu De <dehimangshu03@gmail.com>
2024-02-13 19:29:44 +0530

    Add Notes Page
    > Updated models.py
    > Updated view logic for adding notes and handling requests.
    > Added Styling to the Add Notes page.[add_note.css, add_note.html]
    
    ** Migration files are present due to model creation and updation.

--- notes/views.py
+++ notes/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 from .models import Notes
 
 # Create your views here.
@@ -8,8 +9,13 @@ def home(request):
     return render(request, 'notes/home.html', {"all_notes": all_notes})
 
 def add_note(request):
-    # return HttpResponse("Add Note Page")
+    if request.method == "POST":
+        title = request.POST.get("title")
+        content = request.POST.get("content")
+        note = Notes(title=title, content=content)
+        note.save()
+        return HttpResponseRedirect(reverse("home"))
     return render(request, 'notes/add_note.html')
 
 def edit_note(request, id):
-    return HttpResponse("Edit Note Page")
+    return render(request, "notes/edit_note.html")
