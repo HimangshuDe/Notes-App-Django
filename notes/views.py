06991f974b2d16651fa50adda1f6067462923e1b
Himangshu De <dehimangshu03@gmail.com>
2024-02-13 11:06:30 +0530

    Major Changes
    > Added view logic.
    > Added models for storing notes data in a database
    > Added routes
    > Added templates for each view logics.
    
    ** Migration files are added.

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Notes
 
 # Create your views here.
+def home(request):
+    all_notes = Notes.objects.all()
+    return render(request, 'notes/home.html', {"all_notes": all_notes})
+
+def add_note(request):
+    # return HttpResponse("Add Note Page")
+    return render(request, 'notes/add_note.html')
+
+def edit_note(request, id):
+    return HttpResponse("Edit Note Page")
