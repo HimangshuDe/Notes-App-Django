25b2fe0813bb3d324bb06a6ed9ed03392a3f54c9
HimangshuDe <dehimangshu03@gmail.com>
2024-05-09 12:54:57 +0530

    updated settings urls and vercel files

--- build_file.sh
+++ build_file.sh
@@ -4,4 +4,5 @@ python3.11 -m pip install -r requirements.txt
 python3.11 manage.py collectstatic --noinput --clear
 python3.11 manage.py makemigrations
 python3.11 manage.py migrate
+python3.11 manage.py createsuperuser --noinput --username=admin --email=admin@admin.com
 echo "BUILD END"
